1bfc358d063d699986ac65d6ea948be866a36a0e
miaomiao159 <980249973@qq.com>
2018-05-22 22:36:17 +0800

    add taix code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(mileage,waittime) {
+
+    var price=0;
+    if(mileage>0&&mileage<=2)
+    {
+        price=parseInt(Math.round(6+waittime*0.25));
+    }
+    else if(mileage>2&&mileage<=8)
+    {
+        price=parseInt(Math.round(6+(mileage-2)*0.8+waittime*0.25));
+    }
+    else
+    {
+        price=parseInt(Math.round(6+(8-2)*0.8+(mileage-8)*0.8*(1+0.5)+waittime*0.25));
+    }
+    return price;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,21 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('行程在两公里以内', function() {
+
+        var result = main(2, 1);
+        expect(result).toEqual(6);
+    });
+
+    it('行程在两公里到八公里之间', function() {
+
+        var result = main(8, 3);
+        expect(result).toEqual(12);
+    });
+
+    it('行程在八公里以上', function() {
+
+        var result = main(10, 5);
+        expect(result).toEqual(14);
+    });
 });
